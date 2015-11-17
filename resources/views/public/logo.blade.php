85f7eb68bf403dd39fc9cc7f915a2f0245c4ec98
rafael.sisweb <rafael.sisweb@gmail.com>
2015-11-17 17:38:07 -0500

    Logo icone

--- resources/views/public/logo.blade.php
+++ resources/views/public/logo.blade.php
@@ -3,7 +3,7 @@
 @section('body')
 
 <center style="padding-top:160px">
-<a href="{{ NINJA_WEB_URL }}" target="_blank">
+<a href="http://plastivel.com.br" target="_blank">
     <img src="{{ asset('/favicon.png') }}"/>
 </a>
 </center>
